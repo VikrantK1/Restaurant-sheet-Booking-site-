8eaab112e649fb7ea49f3ac2b79799da7071b737
Vikrant Kumar <Vikrantkumar54321@gmail.com>
2020-06-09 08:07:41 +0530

    function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 
 import React  from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
-import Menu from './components/MenuComponent'
-import {DISHES} from './shared/dishes'
+import Main from './components/MainComponent'
 
 function App() {
-  var state={dishes:DISHES}
   return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={state.dishes}/>
-    </div>
+    <Main/>
   );
 }
 
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,53 +1,52 @@
-import React,{Component} from 'react';
+import React from 'react';
 // import * as React from "react";
 import {Card, CardImg, CardBody, CardTitle,Media,CardText} from 'reactstrap';
 
-class DetailedList extends Component{
-
-    render()
-    {
-       
-        const dishDetails=this.props.dishs2;
+function RanderOnecomment({comment}) {
+  return(
+    <div key={comment.id} >
+    <Media tag="li">
+      
+      <Media>{comment.comment}</Media>
+      
+      </Media>
+      <Media tag="li">
+      
+<Media><p>--</p>{comment.author}<p>,</p>{new Intl.DateTimeFormat("en-US",{year:"numeric",month:"short",day:"2-digit"}).format(new Date(Date.parse(comment.date)))}</Media>
+      
+      </Media>
+    
+</div>
+  );
+}  
+    const DetailedList=(props)=>{
+      const dishDetails=this.props.dishs2;
         const dishComments=dishDetails.comments.map((comment)=>{
                return(
-                <div key={comment.id} >
-                    <Media tag="li">
-                      
-                      <Media>{comment.comment}</Media>
-                      
-                      </Media>
-                      <Media tag="li">
-                      
-               <Media><p>--</p>{comment.author}<p>,</p>{new Intl.DateTimeFormat("en-US",{year:"numeric",month:"short",day:"2-digit"}).format(new Date(Date.parse(comment.date)))}</Media>
-                      
-                      </Media>
-                    
-                </div>
+              <RanderOnecomment comment={comment}/>
                );
         });
-        return(
-            <div className="container">
-              <div className="row">
-                  <div className="col-12 col-md-5 m-1">
-                     <Card>
-                      <CardImg src={dishDetails.image}></CardImg>
-                      <CardBody>
-                       <CardTitle>{dishDetails.name}</CardTitle>
-                        <CardText>{dishDetails.description}</CardText>
-                       </CardBody>
-                      </Card>
-                   </div>
-                   <div className="col-12 col-md-5 m-1">
-                     <h2>Comments</h2>  
-                     {dishComments}
-                   </div>
-                </div>
-               
-                </div>
+     return(
+      <div className="container">
+      <div className="row">
+          <div className="col-12 col-md-5 m-1">
+             <Card>
+              <CardImg src={dishDetails.image}></CardImg>
+              <CardBody>
+               <CardTitle>{dishDetails.name}</CardTitle>
+                <CardText>{dishDetails.description}</CardText>
+               </CardBody>
+              </Card>
+           </div>
+           <div className="col-12 col-md-5 m-1">
+             <h2>Comments</h2>  
+             {dishComments}
+           </div>
+        </div>
 
-            
-        );
+        </div>
+     );
+
     }
-} 
 
 export default DetailedList;
